3e6a355e7ba56a2121639c328e0447313fe4c3ea
Andres Bolivar <dre.bolivar@gmail.com>
2022-07-29 12:14:34 -0400

    added a boi

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,9 +4,10 @@ const About = () => {
   return (
     <div className="aboutContainer">
       <h1 className="aboutHeader">About Page</h1>
-      <span className="about">
-        Wow, you really clicked on the About page huh. Well...let's see. I'm a gemini, I into listening to music on vinyl, favorite youtube hobby is mechanical keyboards and lo fi office set up videos. Ummm...I'm pretty sure I licked my elbow once in high school by trying really hard, but these days I'm starting to think that either I implanted that false memory or told myself i reached the elbow when i totally didn't. Does it matter? What are memories? What is existence?
-      </span>
+      {/* <span className="about"> */}
+        Wow, you really clicked on the About page huh. Well...let's see. I'm a gemini, I into listening to music on vinyl, favorite youtube hobby is mechanical keyboards and lo fi office set up videos. Ummm...I'm pretty sure I licked my elbow once in high school by trying really hard, but these days I'm starting to think that either I implanted that false memory or told myself i reached the elbow when i totally didn't. Does it matter? What are memories? What is existence? Here's my dog bucky:
+        <img src="https://i.imgur.com/a2QFk7y.jpg?1"></img>
+      {/* </span> */}
     </div>
   )
 }
