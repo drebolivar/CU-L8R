7ef65152581dbe0c954b0dc393af1823745a7d5d
Andres Bolivar <dre.bolivar@gmail.com>
2022-10-09 17:05:10 -0400

    adjusted sizing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-        <div className="links">
+      <div className="links">
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/"> Moods </Link>
         <Link to="/"> Just Pick For Me! </Link>
-        </div>
+      </div>
+      <div className="title"> CU-L8R </div>
     </header>
   )
 }
