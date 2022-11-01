46971a6f99a7e64ed3bf05cc1b8244cd7e690e3d
Andres Bolivar <dre.bolivar@gmail.com>
2022-11-01 16:22:53 -0400

    about me formatting

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,8 +5,15 @@ const About = () => {
     <div className="aboutContainer">
       <h1 className="aboutHeader">About Page</h1>
       {/* <span className="about"> */}
-        Wow, you really clicked on the About page huh. Well...let's see. I'm a gemini, I into listening to music on vinyl, favorite youtube hobby is mechanical keyboards and lo fi office set up videos. Ummm...I'm pretty sure I licked my elbow once in high school by trying really hard, but these days I'm starting to think that either I implanted that false memory or told myself i reached the elbow when i totally didn't. Does it matter? What are memories? What is existence? Here's my dog bucky:
-        <img src="https://i.imgur.com/a2QFk7y.jpg?1"></img>
+      Wow, you really clicked on the "About" page huh? Well...let's see. I'm a
+      gemini. I'm into listening to music on vinyl, favorite youtube wormholes
+      involve mechanical keyboards and lo-fi office set up videos. Ummm...I'm
+      pretty sure I licked my elbow once in high school by trying really hard,
+      but these days I'm starting to think that either I implanted that false
+      memory or told myself i reached the elbow when i totally didn't. Does it
+      matter? What are memories? What is existence? Who are "we" even? Here's me
+      and my dog Bucky:
+      <img src="https://i.imgur.com/a2QFk7y.jpg?1"></img>
       {/* </span> */}
     </div>
   )
