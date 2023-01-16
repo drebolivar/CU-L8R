2122f6c0d9a8ce4d4828eb1ed422788294aa5263
Andres Bolivar <dre.bolivar@gmail.com>
2023-01-16 17:01:15 -0500

    nav edit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/"> Moods </Link>
-        <Link to="/"> Just Pick For Me! </Link>
+        {/* <Link to="/"> Just Pick For Me! </Link> */}
       </div>
       <img className="title" src={CUL8R} alt="logo" />
     </header>
